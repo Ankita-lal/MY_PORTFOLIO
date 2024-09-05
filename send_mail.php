9b5cdd14a4c19c00879d55aaa4c989f2f309d1b0
Ankita Lal <91656404+Ankita-lal@users.noreply.github.com>
2024-09-05 12:30:04 +0530

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "ankitalal207@gmail.com"; // Your email address
+    $subject = "New Contact Form Submission";
+
+    // Message body
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    // Headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        header("Location: contact.html?status=success");
+        exit();
+    } else {
+        header("Location: contact.html?status=error");
+        exit();
+    }
+}
